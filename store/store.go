d858d63b48b186ca542bd53c7332c9bb99c1f2f4
shaik asif <smd.20sa@gmail.com>
2026-05-24 22:58:59 +0530

    implemented the mutex

--- store/store.go
+++ store/store.go
@@ -1,6 +1,11 @@
 package store
 
+import (
+	"sync"
+)
+
 type Store struct {
+	mu   sync.RWMutex
 	data map[string]string
 }
 
@@ -11,15 +16,22 @@ func NewStore() *Store {
 }
 
 func (s *Store) Set(key, value string) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
 	s.data[key] = value
+
 }
 
 func (s *Store) Get(key string) (string, bool) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
 	value, ok := s.data[key]
 	return value, ok
 
 }
 
 func (s *Store) Del(key string) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
 	delete(s.data, key)
 }
